e96374b535218e7d7482a76d0d18f66af41a7f90
ershova2366 <ershova.n2016@gmail.com>
2022-11-13 21:10:50 +0000

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,3 +3,5 @@ from handlers import test
 from handlers import importExcel
 from handlers import score
 from handlers import dropPatternTest
+from handlers import delete_test
+from handlers import change_test
